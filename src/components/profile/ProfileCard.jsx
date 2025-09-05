8940f1d7125ca92cbf3b16731c71224fbf266ea4
Ayman Mohamed <167534874+lamonaDev@users.noreply.github.com>
2025-09-05 04:38:58 +0300

    finish all
    SOCIAL-APPLICATION

--- src/components/profile/ProfileCard.jsx
+++ src/components/profile/ProfileCard.jsx
@@ -10,8 +10,9 @@ export default function ProfileCard() {
         <>
     {userData && (
     <section className="mx-auto max-w-[900px] w-full">
-        <div className="profile-card-section flex flex-col gap-2 items-center justify-center min-h-[30vh] bg-blue-300 shadow-2xl rounded-2xl p-4 sm:p-6 md:p-8">
-        <div className="relative">
+        <div className="profile-card-section flex flex-wrap flex-row gap-10 items-center justify-evenly min-h-[30vh] bg-blue-300 shadow-2xl rounded-2xl p-4 sm:p-6 md:p-8">
+            <div className="right-section">
+                <div className="relative">
             <Input
             variant="flat"
             color="success"
@@ -39,24 +40,27 @@ export default function ProfileCard() {
         />
         <label
             htmlFor="avatarUpload"
-            className="absolute top-1 left-13 transform -translate-x-1/2 cursor-pointer bg-blue-400 text-black text-sm py-1 px-3 rounded-full hover:bg-blue-600 transition duration-200"
+            className="absolute top-27 md:top-33 left-26 md:left-36 transform -translate-x-1/2 cursor-pointer bg-blue-400 text-black text-sm py-1 px-3 rounded-full hover:bg-blue-600 transition duration-200"
         >+</label>
-        </div>
-        <img
-            onError={(e) => { e.target.src = userPlaceHolder }}
+                </div>
+                <img
+                    onError={(e) => { e.target.src = userPlaceHolder }}
             src={userData?.photo}
             alt="user avatar"
             className="w-30 h-30 sm:w-32 sm:h-32 md:w-40 md:h-40 rounded-full border shadow-2xl object-cover mt-4"
-        />
-        <h2 className="profile-card-user-name font-bold text-2xl sm:text-3xl md:text-4xl text-center">
-            {userData?.name}
-        </h2>
-        <h3 className="profile-card-user-email text-md sm:text-lg md:text-xl">
-            {userData?.email}
-        </h3>
-        <h3 className="profile-card-birth text-md sm:text-lg md:text-xl">
-            {userData?.dateOfBirth.split("T")[0]}
-        </h3>
+                />
+            </div>
+            <div className="left-section flex flex-col justify-center items-center">
+                <h2 className="profile-card-user-name font-bold text-2xl sm:text-3xl md:text-4xl text-center">
+                    {userData?.name}
+                </h2>
+                <h3 className="profile-card-user-email text-md sm:text-lg md:text-xl">
+                    {userData?.email}
+                </h3>
+                <h3 className="profile-card-birth text-md sm:text-lg md:text-xl">
+                    {userData?.dateOfBirth.split("T")[0]}
+                </h3>
+            </div>
         </div>
     </section>
 )}
